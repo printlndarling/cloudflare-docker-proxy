7c152453c6dbf5c3316b94d6abdc875f0ae2cf4d
繁星_逐梦 <116906137+printdarling@users.noreply.github.com>
2024-09-24 10:19:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.cnmirror.us.kg": dockerHub,
+  "quay.cnmirror.us.kg": "https://quay.io",
+  "gcr.cnmirror.us.kg": "https://gcr.io",
+  "k8s-gcr.cnmirror.us.kg": "https://k8s.gcr.io",
+  "k8s.cnmirror.us.kg": "https://registry.k8s.io",
+  "ghcr.cnmirror.us.kg": "https://ghcr.io",
+  "cloudsmith.cnmirror.us.kg": "https://docker.cloudsmith.io",
+  "ecr.cnmirror.us.kg": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.cnmirror.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
